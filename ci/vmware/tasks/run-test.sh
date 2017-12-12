b574a3b0533af0bc9a3c5ff1124609baeb7e7f18
Tushar Aggarwal <taggarwal@vmware.com>
2017-12-12 12:19:17 -0800

    Add run-test.sh to execute integration test on jumpbox

---
+++ ci/vmware/tasks/run-test.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+set -e
+
+source bosh-cpi-src/ci/utils.sh
+source bosh-cpi-src/.envrc
+
+# Source the test environment variables
+pushd vcpi-nimbus
+  source environment.sh
+popd
+
+stemcell_dir="$HOME/stemcell"
+export BOSH_VSPHERE_STEMCELL=${stemcell_dir}/stemcell.tgz
+
+if [ -f /etc/profile.d/chruby.sh ]; then
+  source /etc/profile.d/chruby.sh
+  chruby $PROJECT_RUBY_VERSION
+fi
+
+: ${RSPEC_FLAGS:=""}
+: ${BOSH_VSPHERE_STEMCELL:=""}
+
+# allow user to pass paths to spec files relative to src/vsphere_cpi
+# e.g. ./run-lifecycle.sh spec/integration/core_spec.rb
+if [ "$#" -ne 0 ]; then
+  RSPEC_ARGS="$@"
+fi
+
+install_iso9660wrap() {
+  pushd bosh-cpi-src
+    pushd src/iso9660wrap
+      /usr/local/go/bin/go build ./...
+      export PATH="$PATH:$PWD"
+    popd
+  popd
+}
+
+install_iso9660wrap
+
+pushd bosh-cpi-src/src/vsphere_cpi
+  bundle install
+  bundle exec rspec ${RSPEC_FLAGS} --require ./spec/support/verbose_formatter.rb --format VerboseFormatter spec/integration
+popd

7beca39e1ad00de2f2755d97239db3ce5ecd43a4
Tushar Aggarwal <taggarwal@vmware.com>
2017-12-12 13:04:38 -0800

    Add sudo permission in ci/util.sh

--- ci/utils.sh
+++ ci/utils.sh
@@ -5,8 +5,8 @@ configure_bosh_cli() {
   local bosh_input="$(realpath bosh-cli/*bosh-cli-* 2>/dev/null || true)"
   if [[ -n "${bosh_input}" ]]; then
     export bosh_cli="/usr/local/bin/bosh"
-    cp "${bosh_input}" "${bosh_cli}"
-    chmod +x "${bosh_cli}"
+    sudo cp "${bosh_input}" "${bosh_cli}"
+    sudo chmod +x "${bosh_cli}"
   fi
 }
 configure_bosh_cli
